5180cfb725d629f3f550c6e23cef6b54c15a98ba
Alex Ling <hkalexling@gmail.com>
2016-10-29 16:07:52 +0800

    add publication tab, change favicon and temporally disable error handling

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,24 @@ middleware = require('./middleware'),
 importRoutes = keystone.importer(__dirname);
 
 // Common Middleware
-keystone.pre('routes', middleware.initErrorHandlers);
+// keystone.pre('routes', middleware.initErrorHandlers);
 keystone.pre('routes', middleware.initLocals);
 keystone.pre('render', middleware.flashMessages);
 
 // Handle 404 errors
-keystone.set('404', function(req, res, next) {
-	res.notfound();
-});
+// keystone.set('404', function(req, res, next) {
+// 	res.notfound();
+// });
 
-// Handle other errors
-keystone.set('500', function(err, req, res, next) {
-	var title, message;
-	if (err instanceof Error) {
-		message = err.message;
-		err = err.stack;
-	}
-	res.err(err, title, message);
-});
+// // Handle other errors
+// keystone.set('500', function(err, req, res, next) {
+// 	var title, message;
+// 	if (err instanceof Error) {
+// 		message = err.message;
+// 		err = err.stack;
+// 	}
+// 	res.err(err, title, message);
+// });
 
 // Load Routes
 var routes = {
@@ -31,5 +31,6 @@ var routes = {
 exports = module.exports = function(app) {
 	
 	app.get('/', routes.views.index);
-	
+	app.get('/publication', routes.views.publication);
+
 }
